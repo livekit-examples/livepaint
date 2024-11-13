a8a5c5b62ca21c1dab45b55270c0b1090f99cedc
Paul Wells <paulwe@gmail.com>
2024-11-12 16:40:01 -0800

    swap player/room name in connect form (#2)

--- web/components/ConnectionForm.tsx
+++ web/components/ConnectionForm.tsx
@@ -25,7 +25,7 @@ export function ConnectionForm() {
 
   const onConnectButtonClicked = useCallback(() => {
     localStorage.setItem("playerName", playerName);
-    connect(roomName, playerName);
+    connect(playerName, roomName);
   }, [playerName, roomName, connect]);
 
   return (
